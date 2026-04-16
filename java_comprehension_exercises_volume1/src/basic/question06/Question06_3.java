7858ec5d4d7094f722560a5a6d9d0755a3af7e6b
kiritosh <kiritosh@C010423210.zipangu.cac.co.jp>
2026-04-16 14:59:16 +0900

    question6_3完了。

--- java_comprehension_exercises_volume1/src/basic/question06/Question06_3.java
+++ java_comprehension_exercises_volume1/src/basic/question06/Question06_3.java
@@ -1,13 +1,33 @@
 package basic.question06;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Question06_3 {
-	
+
 	public static void main(String[] args) throws NumberFormatException, IOException {
-		
+
 		// TODO:ここに実装
-		
+		int amount = 0;
+
+		int price = 100;
+
+		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+		System.out.print("鉛筆を購入しますか。");
+		System.out.println("はい：0、いいえ：1 >");
+		String a = reader.readLine();
+		int buyFlag = Integer.parseInt(a);
+
+		while (buyFlag == 0) {
+			amount++;
+			System.out.println("購入する鉛筆の本数を 1 つ増やしますか？");
+			System.out.println("はい：0、いいえ：1 >");
+			String str = reader.readLine();
+			buyFlag = Integer.parseInt(str);
+		}
+		System.out.println("購入した鉛筆の本数は" + amount + "本です");
+
 	}
 
 }
